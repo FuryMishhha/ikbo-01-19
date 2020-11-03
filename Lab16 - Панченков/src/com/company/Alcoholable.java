cd6ecd21aa4955262352bbaa32acb7bd1a51a126
Misha <57438792+FuryMishhha@users.noreply.github.com>
2020-11-03 04:20:06 +0300

    Lab16, Панченков Михаил, ИКБО-01-19

---
+++ Lab16 - Панченков/src/com/company/Alcoholable.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public interface Alcoholable {
+
+    boolean isAlcoholicDrink();
+
+    double getAlcoholVol();
+}
---
+++ Lab16 - Панченков/src/com/company/OrdersManager.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public interface OrdersManager {
+    int itemsQuantity(String itemName);
+    int itemsQuantity(MenuItem item);
+    Orderable[] getOrders();
+    int ordersCostSummary();
+    int ordersQuantity();
+}
